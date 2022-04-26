269277a2dcac84c76800da3c08f97b7309702525
Javier Sánchez <javier.sanchezortiz@xploregroup.es>
2022-04-26 17:34:42 +0200

    test: Spectator added

--- src/app/pages/tracks/tracks.component.spec.ts
+++ src/app/pages/tracks/tracks.component.spec.ts
@@ -1,25 +1,48 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import * as contentful from 'contentful';
+
+import {
+  createComponentFactory,
+  mockProvider,
+  Spectator,
+} from '@ngneat/spectator';
+import { TranslocoService } from '@ngneat/transloco';
+import { Observable, of, Subject } from 'rxjs';
+import { tracks } from 'src/app/spec-helpers/track.spec-helper';
+import { TracksService } from './services/tracks.service';
 
 import { TracksComponent } from './tracks.component';
+import { Track } from './models/track';
 
 describe('TracksComponent', () => {
-  let component: TracksComponent;
-  let fixture: ComponentFixture<TracksComponent>;
+  let spectator: Spectator<TracksComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ TracksComponent ]
-    })
-    .compileComponents();
+  const createComponent = createComponentFactory({
+    component: TracksComponent,
+    shallow: true,
+    providers: [
+      mockProvider(TracksService, {
+        tracks$: of(tracks.items),
+      }),
+      mockProvider(TranslocoService, {
+        langChanges$: of('es'),
+      }),
+    ],
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TracksComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  beforeEach(async () => {
+    spectator = createComponent({
+      detectChanges: false,
+    });
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    spectator.detectChanges();
+    expect(spectator.component).toBeTruthy();
+  });
+
+  it('should render track cards', () => {
+    spectator.detectChanges();
+    const trackCards = spectator.queryAll('mat-card');
+    expect(trackCards.length).toBe(tracks.items.length);
   });
 });
