9b9b13cf26e9be804b1d951767eed6b6487fae41
Javier Sánchez <javier.sanchezortiz@xploregroup.es>
2022-04-27 15:11:12 +0200

    test: Tracks service

--- src/app/pages/tracks/services/tracks.service.spec.ts
+++ src/app/pages/tracks/services/tracks.service.spec.ts
@@ -1,16 +1,35 @@
-import { TestBed } from '@angular/core/testing';
+import { createServiceFactory, SpectatorService } from '@ngneat/spectator';
+import { tracks } from 'src/app/spec-helpers/track.spec-helper';
 
 import { TracksService } from './tracks.service';
 
-describe('TracksService', () => {
-  let service: TracksService;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(TracksService);
+describe('AuthService', () => {
+  let spectator: SpectatorService<TracksService>;
+  const createService = createServiceFactory({
+    service: TracksService,
   });
 
+  beforeEach(() => (spectator = createService()));
+
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(spectator.service).toBeTruthy();
+  });
+
+  it('should return tracks and tracks$ have been called', async () => {
+    const tracksService = spectator.service as any;
+    spyOn(tracksService.cdaClient, 'getEntries').and.returnValue(tracks as any);
+    spyOn(tracksService.tracks$, 'next');
+    await tracksService.getTracks();
+    expect(tracksService.cdaClient.getEntries).toHaveBeenCalled();
+    expect(tracksService.tracks$.next).toHaveBeenCalled();
+  });
+
+  it('should return a track and track$ have been called', async () => {
+    const tracksService = spectator.service as any;
+    spyOn(tracksService.cdaClient, 'getEntries').and.returnValue(tracks as any);
+    spyOn(tracksService.track$, 'next');
+    await tracksService.getTrack('test-slug');
+    expect(tracksService.cdaClient.getEntries).toHaveBeenCalled();
+    expect(tracksService.track$.next).toHaveBeenCalled();
   });
 });
