92607714564871822bc3194eefbae0efafbb60ef
Javier Sánchez <javier.sanchezortiz@xploregroup.es>
2022-04-27 17:12:51 +0200

    test: Slider component tests added

--- src/app/pages/slider/slider.component.spec.ts
+++ src/app/pages/slider/slider.component.spec.ts
@@ -1,25 +1,30 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Spectator, createComponentFactory } from '@ngneat/spectator';
+import { tracks } from 'src/app/spec-helpers/track.spec-helper';
+import { SwiperModule } from 'swiper/angular';
 import { SliderComponent } from './slider.component';
 
 describe('SliderComponent', () => {
-  let component: SliderComponent;
-  let fixture: ComponentFixture<SliderComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ SliderComponent ]
-    })
-    .compileComponents();
+  let spectator: Spectator<SliderComponent>;
+  const createComponent = createComponentFactory({
+    component: SliderComponent,
+    shallow: true,
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SliderComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    spectator = createComponent({
+      props: { slides: tracks.items[0].fields.images as any },
+    });
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should be truthy', () => {
+    expect(spectator.component).toBeTruthy();
+  });
+
+  it('should be set the images', () => {
+    spectator.detectChanges();
+    const imageArr = [
+      `${tracks.items[0].fields.images[0].fields.imageFile.fields.file.url}?w=768`,
+    ];
+    expect(spectator.component.imageSlides).toEqual(imageArr);
   });
 });
